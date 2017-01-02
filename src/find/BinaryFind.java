26a610aed9a0035dd34afcf5278ecbd7e318211c
tanghaozheng <tanghaozheng@gmail.com>
2017-01-02 22:49:42 +0800

    binary find commit

--- src/find/BinaryFind.java
+++ src/find/BinaryFind.java
@@ -1,10 +1,21 @@
 package find;
 
 public class BinaryFind {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	// 二分查找
+	public static int binaryFind(int[] arr, int num) {
+		int low = 0;
+		int hight = arr.length - 1;
+		int mid;
+		while (low <= hight) {
+			mid = (low + hight) / 2;
+			if (arr[mid] == num) {
+				return mid + 1;
+			} else if (arr[mid] < num) {
+				low = mid + 1;
+			} else {
+				hight = mid - 1;
+			}
+		}
+		return -1;
 	}
-
 }
